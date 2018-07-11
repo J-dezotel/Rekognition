693cb2384d686b31f540504ff62a08235aea9f83
jay_dezotel <juldez0890@gmail.com>
2018-07-11 07:04:14 -0500

    Clarifai
    Integrated the Clarifai API. currently just renders a stock image and doesn't display face detection boxes. Custom detection based on any image loaded coming soon!

--- rekognition/src/App.js
+++ rekognition/src/App.js
@@ -5,6 +5,12 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+  apiKey: 'bb7d20b56e764451914105df6c224add'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +26,30 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('click');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,9 +59,9 @@ class App extends Component {
         <Navigation />
          <Logo />
          <Rank />
-         <ImageLinkForm />
-         {// <FaceRekognition />
-       }
+         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+         <FaceRecognition />
+       
        </div>
     );
   }
